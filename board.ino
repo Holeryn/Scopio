5a55fc5861bf3da0a85ebfdefb469eeaff19980a
Holeryn <53957646+Holeryn@users.noreply.github.com>
2020-01-11 17:35:38 +0100

    Update board.ino

--- board.ino
+++ board.ino
@@ -1,25 +1,3 @@
-/*
-  WiFi Web Server LED Blink
-
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 9.
-
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the WiFi.begin() call accordingly.
-
- Circuit:
- * WiFi shield attached
- * LED attached to pin 9
-
- created 25 Nov 2012
- by Tom Igoe
- */
 #include <SPI.h>
 #include <WiFi101.h>
 #include <MKRMotorCarrier.h>
@@ -30,7 +8,6 @@
 
 #define MOTOR_ROATATION 25
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;                 // your network key Index number (needed only for WEP)
